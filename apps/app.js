e5975f10423e46561ddc6602f83bf0d2534bbc5a
LucyMac <alessonadaykeepstheboredomaway@gmail.com>
2015-05-14 00:36:20 +0100

    fixed bug

--- apps/app.js
+++ apps/app.js
@@ -1,11 +1,16 @@
 $(function(){     
 	var youTubeAPI = 'https://www.googleapis.com/youtube/v3/search';
-	$.getJSON(youTubeAPI, function(data){
-    	part: 'snippet'
-    	key: 'AIzaSyAqGGJTTcrXv0OppL-uc0WzN_GZOMajB9Q'
-    	q: $('#query').val();
-	})
-	.done(function(data){
-		console.log(data);
+	$('#search-term').submit(function(event){
+		event.preventDefault();
+		var searchTerm = $('#query').val();
+		$.getJSON(youTubeAPI, {
+	    	part: 'snippet',
+	    	key: 'AIzaSyAqGGJTTcrXv0OppL-uc0WzN_GZOMajB9Q',
+	    	q: searchTerm
+		}, function(data){
+        	$.each(data.items, function(index, value){
+				console.log(value);
+			});
+		});
 	});
 });
